fcb9ad6fe5ba0aa967f9a077628f3b66a5e0191c
steve <steve.hamblett@linux.com>
2015-05-20 14:11:12 +0100

    Hurrah, output from the Dart VM

--- mod_dart.c
+++ mod_dart.c
@@ -9,8 +9,27 @@
 #include "http_config.h"
 #include "http_log.h"
 #include "http_protocol.h"
+#include "apr_hash.h"
+#include "ap_config.h"
+#include "ap_provider.h"
+#include "apr_strings.h"
 
 #include "mod_dart.h"
+
+typedef struct {
+   
+    const char *pathToExe;    
+    
+}md_config;
+
+static md_config config;
+
+
+const char *md_set_exe_path(cmd_parms *cmd, void *cfg, const char *arg)
+{
+    config.pathToExe = arg;
+    return (const char*)NULL;
+}
 
 //static void md_child_init(apr_pool_t *p, server_rec *s) {
 
@@ -18,13 +37,31 @@
 
 static int md_handler(request_rec *r) {
  
+    char* filename;
+    FILE *fp;
+    int status;
+    char output[PATH_MAX];
+    char command[PATH_MAX];
+    
     if (!r->handler || strcmp(r->handler,"dart")) return (DECLINED);
     
-    /* Now that we are handling this request, we'll write out "Hello, world!" to the client.
-     * To do so, we must first set the appropriate content type, followed by our output.
+    /**
+     * We must first set the appropriate content type, followed by our output.
      */
     ap_set_content_type(r, "text/html");
-    ap_rprintf(r, "Hello, world!");
+    filename = apr_pstrdup(r->pool, r->filename);
+    strcat(config.pathToExe, "  ");
+    strcat(config.pathToExe, filename);
+    ap_rprintf(r, "<h2> POPEN %s</h2>",config.pathToExe);
+    fp = popen(config.pathToExe, "r");
+    if (fp == NULL) {
+    
+        ap_rprintf(r, "<h2> POPEN Fail %s</h2>", config.pathToExe);
+    }
+    
+    while (fgets(output, PATH_MAX, fp) != NULL)
+    ap_rprintf(r, "%s", output);
+    status = pclose(fp);
     
     /* Lastly, we must tell the server that we took care of this request and everything went fine.
      * We do so by simply returning the value OK to the server.
@@ -41,7 +78,7 @@ static void md_register_hooks(apr_pool_t *p) {
 
 static const command_rec md_directives[] = {
     //AP_INIT_TAKE1("DartDebug", (cmd_func) dart_set_debug, NULL, OR_ALL, "Whether error messages should be sent to the browser"),
-    
+    AP_INIT_TAKE1("DartExePath",  md_set_exe_path, NULL, RSRC_CONF, "The path to the Dart executable"),
     { NULL }
 };
 
