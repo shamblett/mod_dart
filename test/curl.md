79a9f21750a4075e9b6e5edaf5ca9497892dc749
steve <steve.hamblett@linux.com>
2015-06-08 13:21:12 +0100

    Documentation

---
+++ test/curl.md
@@ -0,0 +1,5 @@
+# CURL Testing command examples 
+
+```
+curl -i -d "computer=mac&monitor=dell" -b "size=xxx+large; colour=red" -X POST  http://localhost/index.dart?car=audi
+```
--- test/index.dart
+++ test/index.dart
@@ -1,18 +1,22 @@
-
 import 'dart:io';
 import 'dart:convert';
 
 void main() {
 
- 
+  // Set a header
+  Apache.setHeader(Apache.CONTENT_LANGUAGE, 'de');
+
+  // Dump the environment depending on format selection
   if ( Apache.Get['format'] == 'json' ) {
-	Apache.setHeader(Apache.CONTENT_TYPE, "application/json");
-	Apache.dumpEnvironmentJSON();
+        Apache.setHeader(Apache.CONTENT_TYPE, "application/json");
+        Apache.dumpEnvironmentJSON();
   } else {
-  	Apache.setHeader(Apache.CONTENT_TYPE, "text/html");
-	Apache.dumpEnvironment();
+        Apache.setHeader(Apache.CONTENT_TYPE, "text/html");
+        Apache.dumpEnvironment();
   }
 
+  // Flush buffers an exit
   Apache.flushBuffers();
+
 	  
 }
